0716f36247168a00377fdc62c415ff4b22d4f46a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-21 19:30:49 -0500

    style: js-beautify formatting (frontend/rewrites.js)

--- frontend/rewrites.js
+++ frontend/rewrites.js
@@ -1,38 +1,42 @@
- const updateAttribute = (el,k,v)=>{
-        try{
-          const val = el.getAttribute(k);
-          if(val != v){
-            el.setAttribute(k,v);
-          }
-        }catch(e){
-          console.warn(e,el,k,v);
-        } 
-      };
-      const hostTargets = ${JSON.stringify(hostTargets)};
-      for(const host of hostTargets){
-        [...document.querySelectorAll('a[href]')].forEach(x=>updateAttribute(x,'href',x.href.replace(host,location.host)));
-      }
-        const ie = [...document.querySelectorAll('img')];
-        for(const i of ie){
-          i.onerror =(()=>{ i.src='https://image-gen.lenguapedia-services.workers.dev?prompt='+String(i.outerHTML);});
-        }
-          const ax = [...document.querySelectorAll('main a[href]')];
-          for(const x of ax){
-            x.href='/wiki/'+x.textContent;
-          }
-          let clicked;
-          document.addEventListener("readystatechange", () => {
-            if(clicked)return;
-            try{
-              [...document.querySelectorAll('main #vector-appearance button')].filter(x=>x.innerText=='hide').map(x=>xclick());
-            }catch{
-              return;
-            }
-            clicked = true;
-          });
-          document.addEventListener("readystatechange", () => {
-              const imgs = [...document.querySelectorAll('figure:has(img:not([loaded="true"]))')];
-              for(const i of imgs){
-                (i.querySelector('img')??{}).src+=String(i.textContent||i.innerText);
-              }
-          });
+ const updateAttribute = (el, k, v) => {
+   try {
+     const val = el.getAttribute(k);
+     if (val != v) {
+       el.setAttribute(k, v);
+     }
+   } catch (e) {
+     console.warn(e, el, k, v);
+   }
+ };
+ const hostTargets = $ {
+   JSON.stringify(hostTargets)
+ };
+ for (const host of hostTargets) {
+   [...document.querySelectorAll('a[href]')].forEach(x => updateAttribute(x, 'href', x.href.replace(host, location.host)));
+ }
+ const ie = [...document.querySelectorAll('img')];
+ for (const i of ie) {
+   i.onerror = (() => {
+     i.src = 'https://image-gen.lenguapedia-services.workers.dev?prompt=' + String(i.outerHTML);
+   });
+ }
+ const ax = [...document.querySelectorAll('main a[href]')];
+ for (const x of ax) {
+   x.href = '/wiki/' + x.textContent;
+ }
+ let clicked;
+ document.addEventListener("readystatechange", () => {
+   if (clicked) return;
+   try {
+     [...document.querySelectorAll('main #vector-appearance button')].filter(x => x.innerText == 'hide').map(x => xclick());
+   } catch {
+     return;
+   }
+   clicked = true;
+ });
+ document.addEventListener("readystatechange", () => {
+   const imgs = [...document.querySelectorAll('figure:has(img:not([loaded="true"]))')];
+   for (const i of imgs) {
+     (i.querySelector('img') ?? {}).src += String(i.textContent || i.innerText);
+   }
+ });
