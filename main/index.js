9bcff58a30bf05a79f8cd04b5016457bcd84998e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-23 08:23:15 -0500

    Update index.js

--- main/index.js
+++ main/index.js
@@ -122,8 +122,8 @@ globalThis.onRequest = async (request, env, ctx) => {
           headers: request.headers,
         }),
       ]);
-      let art1 = articles[0]?.split?.(/<main[^>]+>/)?.[1]?.split?.("</main>")?.[0] || articles[0];
-      let art2 = articles[1]?.split?.(/<main[^>]+>/)?.[1]?.split?.("</main>")?.[0] || articles[1] || articles[0];
+      let art1 = articles[0]?.split?.(/<main[^>]*>/)?.[1]?.split?.("</main>")?.[0] || articles[0];
+      let art2 = articles[1]?.split?.(/<main[^>]*>/)?.[1]?.split?.("</main>")?.[0] || articles[1] || articles[0];
       return new Response(
         articles[0]
           .split(/<main[^>]+>/)[0]
