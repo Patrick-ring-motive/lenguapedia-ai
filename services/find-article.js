a8bb29d6bbe8dd19b054a956fc6e1b9bfb62e99f
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-26 10:47:39 -0500

    Update find-article.js

--- services/find-article.js
+++ services/find-article.js
@@ -8,7 +8,6 @@ if (typeof env === "undefined") {
   globalThis.env = {};
 }
 
-const contentScripts = ``;
 
 const fetchResponse = async (...args) => {
   try {
@@ -21,108 +20,48 @@ const fetchResponse = async (...args) => {
   }
 };
 
-const fetchText = async (...args) => {
-  try {
-    return await (await fetchResponse(...args)).text();
-  } catch (e) {
-    return String(e);
-  }
-};
-
-const onLengRequestText = async (...args) => {
-  try {
-    return await (await onLengRequest(new Request(...args))).text();
-  } catch (e) {
-    return String(e);
-  }
-};
-
-const escapeRegExp = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 globalThis.onRequest = async (request, env, ctx) => {
-  try {
     const referHost = request.headers.get('referer') && new URL(request.headers.get('referer')).host;
-    const localhost = new URL(request.url).host;
-    if (referHost && referHost !== localhost) {
+    const url = new URL(request.url);
+    const title = url.searchParams.get('title');
+    const reqHost = url.host;
+    /*if (referHost && referHost !== reqHost) {
       return new Response(null, {
         status: 400
       });
-    }
-    const urlparts = request.url.split("/");
-    const title1 = decodeURIComponent(urlparts[4]).replaceAll("_", " ");
-    const combined =
-      `${decodeURIComponent(urlparts[4])} ${decodeURIComponent(urlparts[5])}`.replaceAll(
-        "_",
-        " ",
-      );
-    let regres = await onLengRequest(request, env, ctx);
-    if (regres.status >= 400) {
-      try {
-        const loc =
-          new URL(request.url).origin +
-          "/merge/" +
-          (
-            await getTop2WikipediaTitles(String(request.url.split("wiki")[1]))
-          ).join("/");
-        return new Response(null, {
-          status: 302,
-          headers: {
-            location: loc,
-          },
-        });
-      } catch (e) {
-        console.warn(e, request.url);
+    }*/
+
+    const reqInit = {
+      headers: {'user-agent':'lenguapedia','cahce-control':'no-cache'},
+    };
+
+    let res, req;
+    for (const host of hostTargets) {
+      url.host = host;
+      url.pathname = `/wiki/${title}`;
+      url.search='';
+      console.log(String(url));
+      if (request.body) {
+        reqInit.body = request.body;
+      }
+      req = new Request(String(url), reqInit);
+      res = await fetchResponse(req);
+      if (/^2/.test(res.status)) {
+        break;
       }
     }
-    return regres;
-  } catch (e) {
-    return new Response(String(e), {
-      status: 500,
-      statusText: String(e),
-    });
-  }
+    if(!/^2/.test(res.status)){
+      const newTitle = await getTopWikiTitle(title);
+      res = await fetch(`https://en.wikipedia.org/wiki/${newTitle}`,reqInit);
+    }
+    return res;
 };
 
 export const onRequest = globalThis.onRequest;
 
-async function onLengRequest(request, env, ctx) {
-  const url = new URL(request.url);
-  const reqHost = url.host;
-  let res, req;
-  for (const host of hostTargets) {
-    url.host = host;
-    const reqInit = {
-      method: request.method,
-      headers: request.headers,
-    };
-    if (request.body) {
-      reqInit.body = request.body;
-    }
-    req = new Request(String(url), reqInit);
-    res = await fetchResponse(req);
-    if (/^[23]/.test(res.status)) {
-      break;
-    }
-  }
-  if (/script|text|json|html|xml/i.test(res.headers.get("content-type"))) {
-    let bodyText = await res.text();
-    for (const host of hostTargets) {
-      bodyText = bodyText.replaceAll(host, reqHost);
-    }
-    if (/json/i.test(res.headers.get("content-type"))) {
-      try {
-        bodyText = JSON.stringify(JSON.parse(bodyText), null, 2);
-      } catch {}
-    }
-    if (/html/i.test(res.headers.get("content-type"))) {
-      bodyText += contentScripts;
-    }
-    res = new Response(bodyText, res);
-  }
-  return res;
-}
 
-async function getTop2WikipediaTitles(query) {
+async function getTopWikiTitle(query) {
   query = decodeURIComponent(String(query)).replaceAll(/[^a-zA-Z]/g, " ");
   const res = await fetch(
     `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(query)}&srlimit=2&format=json&origin=*`, {
@@ -133,5 +72,10 @@ async function getTop2WikipediaTitles(query) {
   );
 
   const data = await res.json();
-  return (data.query?.search || []).map((result) => result.title);
+  return (data.query?.search || []).map((result) => result.title)[0];
 }
+export default {
+  async fetch(request, env, ctx) {
+    return onRequest(request,env,ctx);
+  }
+};
