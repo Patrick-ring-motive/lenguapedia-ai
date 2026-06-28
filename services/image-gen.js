93a2ecf6469dc6f30c1c860f9fbbbe017ed6cc19
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-27 20:06:12 -0500

    Update image-gen.js

--- services/image-gen.js
+++ services/image-gen.js
@@ -90,12 +90,11 @@ export async function onRequest(request, env, ctx) {
     const imgBytes = [...await fetchBytes(image)];
     const imgInput = {
       image: imgBytes,
-      prompt: "Generate a detailed prompt with which I can recreate this image",
+      prompt: "Generate a prompt with which I can recreate this image",
       max_tokens: 512,
     };
-    //const response = await env.AI.run("@cf/llava-hf/llava-1.5-7b-hf",imgInput);
-    //inputs.prompt += JSON.stringify(response);
-    inputs.prompt = new Uint8Array(imgBytes).toBase64();
+    const response = await env.AI.run("@cf/llava-hf/llava-1.5-7b-hf",imgInput);
+    inputs.prompt = JSON.stringify({...response,prompt});
   }
 
   
