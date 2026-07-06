99cb3ff88e103f886599bf7f47c7642ed52d94e8
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-07-06 12:28:34 -0500

    Update index.js

--- main/index.js
+++ main/index.js
@@ -195,7 +195,7 @@ globalThis.onRequest = async (request, env, ctx) => {
   try {
     const referHost = request.headers.get('referer') && new URL(request.headers.get('referer')).host;
     const localhost = new URL(request.url).host;
-    if (referHost && referHost !== localhost) {
+    if ((referHost && (referHost !== localhost))||(request.headers.get('x-provider-key')!==env.PROVIDER_KEY)) {
       return new Response(null, {
         status: 400
       });
